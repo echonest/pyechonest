e6c7e18f5c2e5415e1a8d71da4a63263c0b03c4e
paul.lamere <paul.lamere@ef99729e-6296-11de-9adf-53a34ab33f75>
2009-07-16 11:33:52 +0000

    Some code cleanup

--- examples/wanderer.py
+++ examples/wanderer.py
@@ -20,36 +20,34 @@ around the artist neighborhood.
 
 usage = """
 usage:
-    python wander.py artist name
+    python wander.py max artist name
 """
-# How to set your ECHO_NEST_API_KEY:
-#   1) Set the environment variable 'ECHO_NEST_API_KEY' to be your key, or
-#   2) Uncomment the next line and add your key between the quotes
-#config.ECHO_NEST_API_KEY="YOUR API KEY HERE" 
-
-def wander(band, max=15):
-    count = 0
-    while count < max:
-        if band.audio():
-            audio = random.choice(band.audio())
-            play(audio)
-            count += 1
-        band = random.choice(band.similar())
+def wander(seed, max=10):
+    """ generate a playlist by wandering a seed artist neighborhood """
+    played = []
+    while max:
+        if seed.audio():
+            audio = random.choice(seed.audio())
+            if audio['url'] not in played:
+                play(audio)
+                played.append(audio['url'])
+            max -= 1
+        seed = random.choice(seed.similar())
 
 def play(audio):
     if 'title' in audio and 'artist' in audio and 'url' in audio:
         print audio['title'],  "by", audio['artist'] 
-        print "   ", audio['url']
-        print
+        print "   ", audio['url'], "\n"
 
 
 if __name__ == '__main__':
-    if len(sys.argv) >= 2:
-        args = sys.argv[1:]
+    if len(sys.argv) >= 3:
+        max = int(sys.argv[1])
+        args = sys.argv[2:]
         name = ' '.join(args)
         alist = artist.search_artists(name)
         if (len(alist) > 0):
-            wander(alist[0])
+            wander(alist[0], max)
         else :
             print "Can't find ", name
     else:
