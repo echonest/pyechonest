4537b98e2813759e9abf54a897e0505284b6f489
williams.tyler@gmail.com <williams.tyler@gmail.com@ef99729e-6296-11de-9adf-53a34ab33f75>
2011-05-02 13:20:37 +0000

    add back in until we fix remix

--- pyechonest/config.py
+++ pyechonest/config.py
@@ -33,6 +33,12 @@ You may change this to be a user agent string of your
 own choosing
 """
 
+MP3_BITRATE = 128
+"""		
+Default bitrate for MP3 output. Conventionally an		
+integer divisible by 32kbits/sec.		
+"""
+
 TRACE_API_CALLS = False
 """
 If true, API calls will be traced to the console
